24085163bebb00874a16296e833bb244a5dd405b
Yazan Eldisi <yazaneldisi@Yazans-MBP.home>
2022-07-12 12:25:22 -0500

    testing DOM load

--- static/app.js
+++ static/app.js
@@ -1,47 +1,49 @@
-allForms = document.querySelectorAll(".form");
-let storePics = [];
-async function getFavData(val, title, id, len) {
-  const res = await axios.post(
-    "https://video-game-extract.herokuapp.com/get_fav",
-    {
-      fav_input: val,
-      fav_name: title,
-      api_Id: id,
-    }
-  );
-  return res.data;
-}
-async function deleteFavData(val, title) {
-  const res = await axios.post(
-    "https://video-game-extract.herokuapp.com/delete_fav",
-    {
-      fav_input: val,
-      fav_name: title,
-    }
-  );
-  return res.data;
-}
+document.addEventListener("DOMContentLoaded", function (event) {
+  allForms = document.querySelectorAll(".form");
+  let storePics = [];
+  async function getFavData(val, title, id, len) {
+    const res = await axios.post(
+      "https://video-game-extract.herokuapp.com/get_fav",
+      {
+        fav_input: val,
+        fav_name: title,
+        api_Id: id,
+      }
+    );
+    return res.data;
+  }
+  async function deleteFavData(val, title) {
+    const res = await axios.post(
+      "https://video-game-extract.herokuapp.com/delete_fav",
+      {
+        fav_input: val,
+        fav_name: title,
+      }
+    );
+    return res.data;
+  }
 
-for (let form of allForms) {
-  let input = form.querySelector(".input");
-  let title = input.parentElement.parentElement.innerText;
-  let apiId = parseInt(
-    input.parentElement.parentElement.parentElement.parentElement.parentElement
-      .parentElement.id
-  );
-  let len =
-    input.parentElement.parentElement.parentElement.parentElement.parentElement
-      .children[2].children;
-  form.addEventListener("click", () => {
-    if (input.checked == true) {
-      console.log("Turned on");
-      console.log(len[0]);
-      getFavData(input.value, title, apiId, len);
-    }
-    if (input.checked == false) {
-      console.log(input.value);
-      console.log(title);
-      deleteFavData(input.value, title);
-    }
-  });
-}
+  for (let form of allForms) {
+    let input = form.querySelector(".input");
+    let title = input.parentElement.parentElement.innerText;
+    let apiId = parseInt(
+      input.parentElement.parentElement.parentElement.parentElement
+        .parentElement.parentElement.id
+    );
+    let len =
+      input.parentElement.parentElement.parentElement.parentElement
+        .parentElement.children[2].children;
+    form.addEventListener("click", () => {
+      if (input.checked == true) {
+        console.log("Turned on");
+        console.log(len[0]);
+        getFavData(input.value, title, apiId, len);
+      }
+      if (input.checked == false) {
+        console.log(input.value);
+        console.log(title);
+        deleteFavData(input.value, title);
+      }
+    });
+  }
+});
