38cc52a4097961b11771eda5ea52d172bd83d8d4
Yazan Eldisi <yazaneldisi@Yazans-MBP.home>
2022-07-12 15:57:55 -0500

    Test

--- static/game-home.js
+++ static/game-home.js
@@ -1,7 +1,9 @@
 let delForm = document.querySelectorAll(".home_del_form");
 document.addEventListener("DOMContentLoaded", function (event) {
   for (let btn of delForm) {
-    setTimeout(btn.setAttribute("class", ""), 500000);
+    setTimeout(() => {
+      btn.setAttribute("class", "");
+    }, "5000");
   }
 });
 window.addEventListener("pageshow", function (event) {
