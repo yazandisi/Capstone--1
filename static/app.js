4d34fd9937e5529e21d057523ec298f3609a7e52
Yazan Eldisi <yazaneldisi@Yazans-MBP.home>
2022-07-14 00:22:58 -0500

    Test

--- static/app.js
+++ static/app.js
@@ -1,3 +1,11 @@
+jQuery(document).ready(function ($) {
+  $(window).load(function () {
+    setTimeout(function () {
+      $("#preloader").fadeOut("slow", function () {});
+    }, 2000); // set the time here
+  });
+});
+
 document.addEventListener(
   "DOMContentLoaded",
   setTimeout(function (event) {
